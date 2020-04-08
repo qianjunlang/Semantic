2c074b2a99e70b8d27136a9802cf5e84602cf58f
qianjunlang <498267666@qq.com>
2020-04-08 21:10:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,25 +2,28 @@ from src.semantic import Analysis
 
 if __name__=='__main__':
 	
-	text = '厦门明天会不会下雨'
+	text = '自然语言处理是什么？'
 
 	slu = Analysis(text)
 	
-	data = slu.analysis()  #解析
-	print(data) #总的结果
+	#data = slu.analysis()  #解析
+	#print(data) #总的结果
 	
-	print('--------------------------')
+	#print('--------------------------')
 	
-	print(slu.cws) #分词
+	#print(slu.cws) #分词
 	
-	print(slu.pos) #词性标注
+	#print(slu.pos) #词性标注
 		
+	print("NER")
 	print(slu.ner) #命名实体识别
 	
-	print(slu.domain) #领域分类
+	#print(slu.domain) #领域分类
 	
+	print("意图识别")
 	print(slu.intent) #意图识别
 	
+	print("槽位填充")
 	print(slu.slot) #槽填充
 	
 	
